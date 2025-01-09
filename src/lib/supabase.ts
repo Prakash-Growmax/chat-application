bf34adde24de308b21513fff5195dec42a1c8f71
Prakash Raj <prakashraj@apptino.com>
2025-01-09 13:12:17 +0530

    fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const BaseURL = import.meta.env.VITE_SUPABASE_URL;
-console.log("🚀 ~ BaseURL:", BaseURL);
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
 
 console.log("ENV Variables:", {
   supabaseUrl,
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
+import { viteEnvs } from "vite-envs";
 
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "");
-  console.log("🚀 ~ defineConfig ~ env:", env);
-
   return {
-    plugins: [react()],
+    plugins: [react(), viteEnvs()],
     build: {
       chunkSizeWarningLimit: 1200,
     },
@@ -20,12 +16,6 @@ export default defineConfig(({ mode }) => {
     },
     optimizeDeps: {
       exclude: ["lucide-react"],
-    },
-    envPrefix: "VITE_",
-    define: {
-      "import.meta.env.VITE_SUPABASE_URL": `"${process.env.VITE_SUPABASE_URL}"`,
-      "import.meta.env.VITE_SUPABASE_ANON_KEY": `"${process.env.VITE_SUPABASE_ANON_KEY}"`,
-      "import.meta.env.VITE_API_URL": `"${process.env.VITE_API_URL}"`,
     },
   };
 });
